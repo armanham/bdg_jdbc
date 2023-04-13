1281cc14d2f0aec7c098be9f84c0e30fcbaf7ede
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-04-14 00:10:25 +0400

    Updated DQL.java file in sql/

--- src/main/java/com/bdg/Main.java
+++ src/main/java/com/bdg/Main.java
@@ -3,7 +3,10 @@ package com.bdg;
 import com.bdg.db.DBConnectionManager;
 import com.bdg.sql.DQL;
 
+import java.math.BigDecimal;
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.Month;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -12,6 +15,17 @@ public class Main {
 
         DQL.showEmployee(connection);
         System.out.println(DQL.countRowsOfEmployee(connection));
+        DQL.insertIntoEmployee(
+                connection,
+                "Arman",
+                "Male",
+                "Developer",
+                "SoftEng",
+                "BDG",
+                21,
+                LocalDate.of(2002, Month.APRIL, 22),
+                new BigDecimal("200.000")
+                );
 
         DBConnectionManager.closeConnection();
     }
--- src/main/java/com/bdg/sql/DQL.java
+++ src/main/java/com/bdg/sql/DQL.java
@@ -68,37 +68,59 @@ public class DQL {
         }
     }
 
-//    public static boolean insertIntoEmployee(Connection con,
-//                                             String name,
-//                                             String gender,
-//                                             String position,
-//                                             String department,
-//                                             String office,
-//                                             int age,
-//                                             LocalDate startDate,
-//                                             double salary
-//    ) {
-//        checkNullConnection(con);
-//
-//        if (name == null || name.isEmpty() ||
-//                gender == null || !gender.equals("Male") || !gender.equals("Female") ||
-//                position == null || position.isEmpty() ||
-//                department == null || department.isEmpty() ||
-//                office == null || office.isEmpty() ||
-//                age <= 0 || age >= 120 ||
-//                salary <= 0
-//        ) {
-//            throw new IllegalArgumentException("Passed wrong valur for one of the parameters.");
-//        }
-//
-//        try {
-//
-//        } catch (SQLException e) {
-//            System.out.println(e.getMessage());
-//        } finally {
-//
-//        }
-//    }
+    public static int insertIntoEmployee(Connection con,
+                                             String name,
+                                             String gender,
+                                             String position,
+                                             String department,
+                                             String office,
+                                             int age,
+                                             LocalDate startDate,
+                                             java.math.BigDecimal salary
+    ) {
+        checkNullConnection(con);
+
+        if (name == null || name.isEmpty() ||
+                position == null || position.isEmpty() ||
+                department == null || department.isEmpty() ||
+                office == null || office.isEmpty() ||
+                age <= 0 || age >= 120 ||
+                salary.intValue() <= 0
+        ) {
+            throw new IllegalArgumentException("Passed wrong valur for one of the parameters.");
+        }
+        if (gender == null || (!gender.equals("Male") & !gender.equals("Female"))) {
+            throw new IllegalArgumentException("Passed wrong value for parameter 'gender'.");
+        }
+
+        int result = 0;
+
+        try {
+            pst = con.prepareStatement("INSERT INTO employee(" +
+                    "name, gender, position, department, office, age, start_date, salary) " +
+                    "values(?, ?, ? , ?, ?, ?, ?, ?)");
+
+            pst.setString(1, name);
+            pst.setString(2, gender);
+            pst.setString(3, position);
+            pst.setString(4, department);
+            pst.setString(5, office);
+            pst.setInt(6, age);
+            pst.setDate(7, Date.valueOf(startDate));
+            pst.setBigDecimal(8, salary);
+
+            result = pst.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                pst.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return result;
+    }
 
 
     private static void checkNullConnection(Connection con) {
